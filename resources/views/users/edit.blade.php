fddc43eeb67b47831286180e003da2b6147ceb0f
Shakhawat Mollah <sh.sumon99@gmail.com>
2024-04-06 13:00:13 +0600

    laravelcollective/html to spatie/laravel-html in Users

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -23,43 +23,41 @@
         </div>
     @endif
 
-    {!! Form::model($user, ['method' => 'PATCH','route' => ['users.update', $user->id]]) !!}
+    {{ html()->model($user)->form('PATCH',route('users.update', $user->id))->open() }}
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Name:</strong>
-                {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+                {{ html()->text('name', null)->class('form-control')->placeholder('Name') }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Email:</strong>
-                {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+                {{ html()->email('email', null)->class('form-control')->placeholder('Email') }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Password:</strong>
-                {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
+                {{ html()->password('password')->class('form-control')->placeholder('Password') }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Confirm Password:</strong>
-                {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')) !!}
+                {{ html()->password('confirm-password')->class('form-control')->placeholder('Confirm Password') }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Role:</strong>
-                {!! Form::select('roles[]', $roles,$userRole, array('class' => 'form-control','multiple')) !!}
+                {{ html()->select('roles[]', $roles, $userRole)->class('form-control multiple') }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     </div>
-    {!! Form::close() !!}
-
-    <p class="text-center text-primary"><small>Tutorial by LaravelTuts.com</small></p>
+    {{ html()->form()->close() }}
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -34,16 +34,16 @@
                 <td>
                     @if(!empty($user->getRoleNames()))
                         @foreach($user->getRoleNames() as $v)
-                            <label class="badge badge-success">{{ $v }}</label>
+                            <label class="badge bg-primary">{{ $v }}</label>
                         @endforeach
                     @endif
                 </td>
                 <td>
                     <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
                     <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
-                    {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                    {!! Form::close() !!}
+                    {{ html()->form('DELETE', route('users.destroy', $user->id))->open() }}
+                    {{ html()->submit('Delete')->class('btn btn-danger') }}
+                    {{ html()->form()->close() }}
                 </td>
             </tr>
         @endforeach
